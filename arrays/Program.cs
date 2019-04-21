b52193cf9c00f61b2170fa1e07b7e00989b14b58
Vikas Dangwal <ourmac@Vikass-MBP.home>
2019-04-21 19:44:15 -0400

    ReverseArray

--- arrays/Program.cs
+++ arrays/Program.cs
@@ -7,6 +7,42 @@ namespace arrays
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            ReverseArray(1, 2, 3, 4, 5, 6, 7, 8);
+        }
+
+        static void ReverseArray(params int[] input)
+        {
+            if (input.Length == 0)
+                return;
+
+            int frontIndex = 0;
+            int backIndex = input.Length - 1;
+
+            PrintArray(input);
+            while (frontIndex < backIndex)
+            {
+                Swap(input, frontIndex, backIndex);
+                frontIndex++;
+                backIndex--;
+            }
+
+            PrintArray(input);
+        }
+
+        private static void PrintArray(int[] input)
+        {
+            for (int i = 0; i < input.Length; i++)
+            {
+                System.Console.WriteLine(input[i]);
+            }
+        }
+
+        private static void Swap(int[] input, int frontIndex, int backIndex)
+        {
+            int temp = input[frontIndex];
+            input[frontIndex] = input[backIndex];
+            input[backIndex] = temp;
         }
     }
 }
